3e6dfb139c7c08a4207820c84d223ce4c1805aa5
maroaljoker-blip <maroaljoker@gmail.com>
2026-06-06 03:43:37 +0300

    Wave 1 finsed + improving killing buttons
    I just finished the first wave in the game (the easiest one ) and i improved the killing buttons so it only kills what is on the player screen

--- scripts/area_2d4.gd
+++ scripts/area_2d4.gd
@@ -5,4 +5,8 @@ func _on_body_entered(body: Node2D) -> void:
 		await get_tree().create_timer(1.0).timeout
 
 		for s in get_tree().get_nodes_in_group("skeletons left"):
-			s.die()
+			
+			var notifier = s.get_node("VisibleOnScreenNotifier2D")
+			
+			if notifier.is_on_screen():
+				s.die()
--- scripts/grean_button.gd
+++ scripts/grean_button.gd
@@ -1,17 +1,38 @@
 extends Area2D
 
-
-var hits = 0
+var lives := 3
+var can_take_hit := true
 
 @onready var player: CharacterBody2D = $"../Player"
+@onready var label: Label = $Label
 
-func _on_body_entered(body):
+
+
+func _ready():
+	update_label()
+
+func _on_body_entered(body: Node2D) -> void:
 	if body.name == "anything 20" or body.name == "anything 10" or body.name == "anything 30":
-		hits += 1
-		print("Hits:", hits)
-		print(body.name)
-		if hits >= 3:
-			player.die()
 
+		if not can_take_hit:
+			return
+
+		can_take_hit = false
+
+		lives -= 1
+		print("Hit from:", body.name)
+		print("Lives:", lives)
+
+		update_label()
+
+		if lives <= 0:
+			player.die()
 			await get_tree().create_timer(1.0).timeout
 			get_tree().reload_current_scene()
+			return
+
+		await get_tree().create_timer(0.4).timeout
+		can_take_hit = true
+
+func update_label():
+	label.text = "Lives: " + str(lives)
